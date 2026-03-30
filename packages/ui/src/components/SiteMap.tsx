45667b52846529fa2c7aa02461cce1311414a065
Jamie Blair <jab1994@hotmail.co.uk>
2026-03-30 18:16:33 +0100

    fix: remove Leaflet/OSM attribution box from map

--- packages/ui/src/components/SiteMap.tsx
+++ packages/ui/src/components/SiteMap.tsx
@@ -165,9 +165,9 @@ export function SiteMap({
         zoom,
         style: { height: '100%', width: '100%', borderRadius: '8px' },
         ref: mapRef,
+        attributionControl: false,
       },
       React.createElement(TileLayer, {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       }),
       React.createElement(ClickHandler, { onClick: onMapClick }),
